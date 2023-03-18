bbcd0b620a5409970a805df9ecbfa5a618c635bd
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-03-18 13:27:18 -0300

    MaterialTextures

---
+++ Assets/Scripts/Player/PlayerMovementByTransform.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementByTransform : MonoBehaviour
+{
+	[SerializeField, Range(0,20)] private float _minSpeed, _maxSpeed;
+	private float _speed;
+	private float _xAxis, _zAxis;
+
+	private void Awake()
+	{
+		_speed = _minSpeed;
+	}
+
+
+	private void Update()
+	{
+		_xAxis = Input.GetAxisRaw("Horizontal");
+		_zAxis = Input.GetAxisRaw("Vertical");
+
+		if (_xAxis!=0 || _zAxis !=0)
+		{
+			Move(_xAxis,_zAxis);
+		}
+
+		if (Input.GetKeyDown(KeyCode.LeftShift)|| Input.GetKeyDown(KeyCode.RightShift))
+		{
+			_speed = _maxSpeed;
+		}
+		if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+		{
+			_speed = _minSpeed;
+
+		}
+	}
+
+	private void Move(float xAxis, float zAxis)
+	{
+		var dir = transform.right * xAxis + transform.forward * zAxis;
+
+		transform.position += dir * _speed * Time.deltaTime;
+	}
+}
